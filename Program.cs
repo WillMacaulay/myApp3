9183ef4c241beba4c8a9726e7f2a5a7a5896b494
Will Macaulay <will.macaulay@neueda.com>
2021-09-21 16:23:45 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from myApp3
+            !");
+        }
+    }
+}
